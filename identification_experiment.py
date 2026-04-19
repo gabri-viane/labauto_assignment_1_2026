c464d531d3e9980cba71e18d2f594e97b5d4d3ac
Carlizilioli <c.zilioli001@studenti.unibs.it>
2026-04-19 14:46:08 +0100

    ampiezza 50

--- identification_experiment.py
+++ identification_experiment.py
@@ -41,7 +41,7 @@ t = np.arange(0, Duration + Tc, Tc)  # Ensure inclusion of Duration if possible
 
 f0=0.1
 f1=500.0 # Tc=0.001 Fc=1000Hz, Shannon/Nyquist 500Hz
-A=35.0
+A=50.0
 joint_number=0  # array index
 chirp_signal = A*chirp(t, f0=f0, f1=f1, t1=Duration, method='logarithmic')
 
