e2b1295ff9d189639a8c0073e5062466cba2da9f
Carlizilioli <c.zilioli001@studenti.unibs.it>
2026-04-22 12:23:17 +0100

    boh

--- matlab/controllore/controllori_velocita.m
+++ matlab/controllore/controllori_velocita.m
@@ -10,10 +10,11 @@ function [C, notch, P] = carica_controllore(jointNumber,wc,wn, xci_z, xci_p)
     s=tf('s');
     P=tf(modello_continuo);
     wi=wc/20; % pulsazione azione integrale
-    Ti=1/wi
+    Ti=1/wi;
     Co=1+1/(Ti*s);
     notch=(s^2+2*xci_z*wn*s+wn^2)/(s^2+2*xci_p*wn*s+wn^2);
-    Kp=1/abs(freqresp(Co*P*notch,wc))
+    Kp=1/abs(freqresp(Co*P*notch,wc));
     C=Kp*Co;
-    Ki = Kp/Ti
+    Ki = Kp/Ti;
+    fprintf("Joint Number: %d \n Kp=%.2f\tKi=%.2f\n\n",jointNumber,Kp,Ki);
 end
